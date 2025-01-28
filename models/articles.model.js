1bc8f9f33f3afc303f02673e6679b780dce988e7
Imran <imranh@tuta.io>
2025-01-28 14:10:29 +0000

    create endpoint for GET/api/articles with sorted by date (desc), return all articles, update endpoints.json to reflect changes

--- models/articles.model.js
+++ models/articles.model.js
@@ -9,3 +9,11 @@ exports.selectArticleById = (article_id) => {
       return rows[0] || null;
     });
 };
+
+exports.selectArticles = () => {
+  return db
+    .query(
+      `SELECT articles.author , articles.title , articles.article_id , articles.topic , articles.created_at , articles.votes , articles.article_img_url , COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC`
+    )
+    .then(({ rows }) => rows);
+};
