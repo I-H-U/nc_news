f37a614def0b4eabc8decbede7b9ae8cfd1bbaaf
Imran <imranh@tuta.io>
2025-01-30 13:22:22 +0000

    Add sort_by and order query parameters to GET /api/articles endpoint and update endpoints.json documentation

--- models/articles.model.js
+++ models/articles.model.js
@@ -44,9 +44,7 @@ exports.selectArticles = (sort_by, order) => {
   if (!validOrders.includes(order)) {
     return Promise.reject({ status: 400, message: "Bad request" });
   }
-  if (!sort_by) {
-    sort_by = "created_at";
-  }
+
   return db
     .query(
       `SELECT articles.author , articles.title , articles.article_id , articles.topic , articles.created_at , articles.votes , articles.article_img_url , COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY ${sort_by} ${order}`
