20768e02eb25d13d368018e8ba24eeed9c16d3ba
Imran <imranh@tuta.io>
2025-01-31 11:01:02 +0000

    Add comment_count to GET /api/articles/:article_id and update endpoints.json documentation

--- models/articles.model.js
+++ models/articles.model.js
@@ -16,9 +16,15 @@ exports.selectArticleById = (article_id) => {
   return exports
     .checkIfArticleExists(article_id)
     .then(() => {
-      return db.query(`SELECT * FROM articles WHERE articles.article_id = $1`, [
-        article_id,
-      ]);
+      return db.query(
+        `SELECT articles.*, COUNT(comments.comment_id)::INT as comment_count
+        FROM articles
+        LEFT JOIN comments
+        ON articles.article_id = comments.article_id
+        WHERE articles.article_id = $1
+        GROUP BY articles.article_id`,
+        [article_id]
+      );
     })
     .then(({ rows }) => {
       return rows[0] || null;
